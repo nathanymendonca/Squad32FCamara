1da6b25c4368bd99c9e307cc9abf5d7c2304e109
Adriano Santtos <adrianosantosx2@hotmail.com>
2021-04-01 13:23:38 -0300

    início da página do doador

--- src/pages/giver.tsx
+++ src/pages/giver.tsx
@@ -1,4 +1,5 @@
 import { Button } from '../components/Button/Button';
+import { Footer } from '../components/Footer/Footer';
 import { Header } from '../components/Header/Header';
 import styles from '../styles/pages/Giver.module.css';
 
@@ -6,13 +7,28 @@ export default function Giver(){
     return(
         <div className={styles.container}>
             <Header/>
-            <div className={styles.section}>
-            <img src="./icons/book.svg" alt="cover"/>
-            <h1>ESCOLHA A ESCOLA</h1>
-            <div className={styles.choiceBar}></div>
-            </div>
+                <div className={styles.section}>
 
-            <Button text="DOAR"/>
+                    <img src="./icons/book.svg" alt="cover"/>
+                    <h1>ESCOLHA A ESCOLA</h1>
+                    <div className={styles.choiceBar}>
+                        AQUI VAI A ESCOLHA DAS ESCOLAS
+                    </div>
+                    <Button text="BUSCAR"/>
+
+                    <div className={styles.choiceSchool}>
+                        CLIQUE E SELECIONE A ESCOLA
+                    </div>
+                    <img src="/images/example.png" alt="example"/>
+                    <img className={styles.example2} src="/images/example2.png" alt=""/>
+                    <div className={styles.doar}>
+                        <Button text="DOAR"/>
+                    </div>
+
+    
+                
+                </div>
+            <Footer/>
 
         </div>
 
