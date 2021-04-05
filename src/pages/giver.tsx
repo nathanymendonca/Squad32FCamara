b456d0d2ed62609644fde974b13285649bdfd2b3
Adriano Santtos <adrianosantosx2@hotmail.com>
2021-04-05 19:13:58 -0300

    ajuste

--- src/pages/giver.tsx
+++ src/pages/giver.tsx
@@ -1,6 +1,7 @@
 import { Button } from '../components/Button/Button';
 import { Footer } from '../components/Footer/Footer';
 import { Header } from '../components/Header/Header';
+import MaterialTable from 'material-table'
 import styles from '../styles/pages/Giver.module.css';
 import Link from 'next/link'
 
@@ -13,16 +14,67 @@ export default function Giver(){
                     <img src="./icons/book.svg" alt="cover"/>
                     <h1>ESCOLHA A ESCOLA</h1>
                     <div className={styles.choiceBar}>
-                        AQUI VAI A ESCOLHA DAS ESCOLAS
+                        AQUI VAI o SELECT DA ESCOLA
                     </div>
                     <Button text="BUSCAR"/>
 
                     <div className={styles.choiceSchool}>
-                        CLIQUE E SELECIONE A ESCOLA
+                        CLIQUE E SELECIONE A ESCOLA ENTRE AS QUE RETORNARAM
                     </div>
-                    <img src="/images/example.png" alt="example"/>
-                    <img className={styles.example2} src="/images/example2.png" alt=""/>
+                    <br/>
+                    <div>
+
+                    <MaterialTable
+                            
+                            columns={[
+                                { title: 'Aluno', field: 'name' },
+                                { title: 'Matricula', field: 'surname' },
+                                { title: 'Escola', field: 'birthYear',  },
+                                { title: 'Material Solicitado', field: 'birthCity', },
+                                {title: 'Data', field:''},
+                                {title: 'Acessar aviso de recebimento', field:''}
+                            ]}
+                            data={[{ name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 }]}
+                            options={{    
+                                headerStyle: {
+                                    backgroundColor: '#183b9c',
+                                    color: '#FFF'
+                                },
+                                toolbar:false,
+                                paging:false,
+                                sorting:false,
+                                
+                            }}
+                            />
+                    </div>
+                   <br/>
+                   <div>
+
+                    <MaterialTable
+                            
+                            columns={[
+                                { title: 'Aluno', field: 'name' },
+                                { title: 'Matricula', field: 'surname' },
+                                { title: 'Escola', field: 'birthYear',  },
+                                { title: 'Material Solicitado', field: 'birthCity', },
+                                {title: 'Data', field:''},
+                                {title: 'Acessar aviso de recebimento', field:''}
+                            ]}
+                            data={[{ name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 }]}
+                            options={{    
+                                headerStyle: {
+                                    backgroundColor: '#1b7704',
+                                    color: '#FFF'
+                                },
+                                toolbar:false,
+                                paging:false,
+                                sorting:false,
+                                
+                            }}
+                            />
+                    </div>
                     <div className={styles.doar}>
+                        <br/>
                         <Button text="DOAR"/>
 
                         <Link href="/">
